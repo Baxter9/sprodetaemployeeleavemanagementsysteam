955dfdb809c55af186feef74abb4e3c02f75889c
Baxter9 <70960906+Baxter9@users.noreply.github.com>
2023-05-31 08:15:04 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> |SPRODETA Employee Leave Management System| Developed By <a href="https://web.facebook.com/baxterp.gondwe/">Baxter Gondwe</a></p>
+        </div>
+</footer>
